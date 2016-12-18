b9ebeba416ba1b7057eb183ace623decac83d048
Khalid Mohamud <kmohamud1@outlook.com>
2016-12-17 20:30:05 -0800

    commiting work for tuesday bus mall chart

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //               GLOBAL VARIABLES
-
 var chances = 0;
 var allPhotos = [];
 var container = document.getElementById('container');
@@ -10,6 +9,11 @@ var middle = document.getElementById('middle');
 var right = document.getElementById('right');
 var newArray = [];
 var oldArray = [];
+// var showChart = document.getElementById('ShowButton');
+// var hideChart = document.getElementById('HideButton');
+var chartContainer = document.getElementById('chart');
+var buttonContainer = document.getElementById('ButtonContainer');
+//+++++++++++++++++++++++++++++++
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                     NEW PHOTOS OBJECT CONSRUCTER
@@ -33,6 +37,7 @@ function rand() {
 //                  GENERATE 3 RANDOM NON DUPLICATE NUMBERS
 // this makes three random numbers that are not the same in one array and not the
 // same as the three before it and the three after it.
+
 function makeArrayOfThree() {
   oldArray[0] = newArray[0];
   oldArray[1] = newArray[1];
@@ -56,7 +61,7 @@ function makeArrayOfThree() {
 
 function populateImages() {
   makeArrayOfThree();
-  console.log('================');
+  // console.log('================');
   left.src = allPhotos[newArray[0]].path;
   left.alt = allPhotos[newArray[0]].alt;
   allPhotos[newArray[0]].views += 1;
@@ -79,35 +84,46 @@ function populateImages() {
   right.alt = allPhotos[newArray[2]].alt;
   allPhotos[newArray[2]].views += 1;
   // console.log('name: '+allPhotos[newArray[2]].name );
-  // console.log('views: '+allPhotos[newArray[2]].views);
+  // console.log('views: '+allPhotos[newArray [2]].views);
   // console.log('path: '+allPhotos[newArray[2]].path);
   // console.log('clicks: '+allPhotos[newArray[2]].clicks);
   // console.log('====================');
+
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                   EVENT HANDLER
-
 function HandleClick(event) {
+// newItemsArray();
   // event.preventDefault();
+  // console.log(event.target.alt);
   if(event.target.id === 'container') {
     alert('You have to click on an image!');
 
   } else {
-    if(chances <= 25) {
+    if(chances < 25) {
       // console.log(event.target.id);
       for(var i = 0; i < allPhotos.length; i++) {
         if(event.target.alt === allPhotos[i].alt) {
           allPhotos[i].clicks += 1;
+          newItemsArray();
         }
       }
       chances += 1;
-      console.log(event.target.id);
+
+      // console.log(event.target.id);
       populateImages();// CALLS THIS FUNCTION FOR THE SECOND TIME
-    } else {
+
+    } else if (chances === 25) {
+      makeChart();
+      container.removeEventListener('click', HandleClick);
+    }else {
       container.removeEventListener('click', HandleClick);
     }
+
   }
+  //localStorage.setItem('allPhotos', JSON.stringify(allPhotos));
+  //localStorage.setItem('chances', JSON.stringify(chances));
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                 CALL THE PHOTO OBJECTS
@@ -133,49 +149,48 @@ new Photos('wine-glass');
 new Photos('shark');
 new Photos('sweep');
 
-// function makeChart() {
-//   var ctx = document.getElementById("chart").getContext('2d');
-//   var chart = new Chart = (ctx, {
-//     type:'bar'
-//     data: data,
-//     options: options
-//   })
-// };
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// if (localStorage.allPhotos) {
+//   allPhotos = JSON.parse(localStorage.allPhotos);
+// } else {
+//   new Photos('bag');
+//   new Photos('banana');
+//   new Photos('bathroom');
+//   new Photos('boots');
+//   new Photos('breakfast');
+//   new Photos('bubblegum');
+//   new Photos('chair');
+//   new Photos('cthulhu');
+//   new Photos('dog-duck');
+//   new Photos('dragon');
+//   new Photos('pen');
+//   new Photos('pet-sweep');
+//   new Photos('scissors');
+//   new Photos('tauntaun');
+//   new Photos('tentacles');
+//   new Photos('unicorn');
+//   new Photos('water-can');
+//   new Photos('wine-glass');
+//   new Photos('shark');
+//   new Photos('sweep');
 //
 //
-//
-// var data = {
-//     labels: ["January", "February", "March", "April", "May", "June", "July"],
-//     datasets: [
-//         {
-//             label: "My First dataset",
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1,
-//             data: [65, 59, 80, 81, 56, 55, 40],
-//         }
-//     ]
-// };
-//
-//
-// makeChart();
+//   var allPhotosStringified = JSON.stringify(allPhotos);
+//   localStorage.setItem('allPhotos', allPhotosStringified);
+// }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+var itemsClicked = [];
+var itemsName = [];
 
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function newItemsArray() {
+  for (var i = 0 ; i < allPhotos.length; i++){
+    console.log('New items array running');
+    console.log(itemsClicked);
+    itemsClicked[i] = allPhotos[i].clicks;
+    itemsName[i] = allPhotos[i].name;
+  }
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //       CALLS THE IMAGE RENDERING FUNCTION FOR THE FIRST TIME
 
 populateImages();
@@ -183,6 +198,37 @@ populateImages();
 //                EVENT LISTENER
 
 container.addEventListener('click',HandleClick);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function makeChart() {
+
+  console.log('make chart running');
+
+  var ctx = document.getElementById('chart').getContext('2d');
+  var myChart = new Chart(ctx, {
+
+    type: 'bar',
+    data: {
+      labels: itemsName,
+      datasets: [ {
+        label: 'Product Votes',
+        data: itemsClicked,
+        backgroundColor: "rgba(255,153,0,0.6)"
+      }]
+
+    },
+    options: {
+      responsive: false,
+      scales: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    },
+  });
+}
+newItemsArray();
+makeChart();
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                           THE END
